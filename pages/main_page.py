9e844c948b6e2dbad9b8f4fa569bde987cb1d06d
arkuz <arkuzka@mail.ru>
2020-04-10 13:35:11 +0300

    refactored main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -49,31 +49,30 @@ class MainPage(BasePage):
         el.click()
 
 
+    def get_row(self, row_number, delay=1):
+        for attempt in range(0, delay):
+            sleep(1)
+            by, loc = self.main_page_loc.TABLE_ROW
+            rows = self.driver.find_elements(by, f'{loc}[{row_number}]')
+            if rows:
+                return rows[0]
+        return None
+
+
     def get_rows(self, delay=1):
         for attempt in range(0, delay):
             sleep(1)
             rows = self.driver.find_elements(*self.main_page_loc.TABLE_ROW)
             if rows:
                 return rows
-            else:
-                pass
         return None
 
 
     def get_row_data(self, row_number):
-        rows = self.get_rows()
-        if rows is None:
+        row = self.get_row(row_number)
+        if row is None:
             return None
-        row = rows[row_number - 1]
-        name = row.find_element(*self.main_page_loc.NAME_TABLE_ROW).text
-        email = row.find_element(*self.main_page_loc.EMAIL_TABLE_ROW).text
-        time = row.find_element(*self.main_page_loc.TIME_IMG).get_attribute('data-icon')
-        time = True if time == 'check' else False
-        return {
-            'name': name,
-            'email': email,
-            'time': time
-        }
+        return self._create_row_object(row)
 
 
     def get_rows_data(self):
@@ -83,15 +82,19 @@ class MainPage(BasePage):
             return None
         for row_num in range(0, len(rows)):
             row = rows[row_num]
-            name = row.find_element(*self.main_page_loc.NAME_TABLE_ROW).text
-            email = row.find_element(*self.main_page_loc.EMAIL_TABLE_ROW).text
-            time = row.find_element(*self.main_page_loc.TIME_IMG).get_attribute('data-icon')
-            time = True if time == 'check' else False
-            data = {
-                'name': name,
-                'email': email,
-                'time': time
-            }
+            data = self._create_row_object(row)
             rows_list.append(data)
         return rows_list
+
+
+    def _create_row_object(self, row):
+        name = row.find_element(*self.main_page_loc.NAME_TABLE_ROW).text
+        email = row.find_element(*self.main_page_loc.EMAIL_TABLE_ROW).text
+        time = row.find_element(*self.main_page_loc.TIME_IMG).get_attribute('data-icon')
+        time = True if time == 'check' else False
+        return {
+            'name': name,
+            'email': email,
+            'time': time
+        }
 
